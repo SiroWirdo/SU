5d3dce4888e5d83e85c70037081717f7d14f2733
SiroWirdo <sirowirdo@gmail.com>
2016-11-04 21:27:50 +0100

    ILA

--- src/ILA.java
+++ src/ILA.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ILA {
+	private HashMap<String, ArrayList<ArrayList<String>>> uniqCombinations;
+	private ArrayList<String> rules;
+	
+	public ILA(){
+		uniqArguments = new HashMap<String, ArrayList<ArrayList<String>>>();
+	}
+	
+	public void trainILA(Data trainData){
+		int numbComb = 1;
+		HashMap<String, ArrayList<ArrayList<String>>> tempCombinations = new HashMap<String, ArrayList<ArrayList<String>>>();
+		
+		fillUniqArguments(trainData);
 
+		tempCombinations = cloneCombinations(trainData.getUniqClasses());
+	}
+	
+	private void fillUniqArguments(Data data){
+		ArrayList<String[]> arguments = data.getArguments();
+		ArrayList<String> classes = data.getClasses();
+		
+		
+	}
+	
+	private HashMap<String, ArrayList<ArrayList<String>>> cloneCombinations(ArrayList<String> classes){
+		HashMap<String, ArrayList<ArrayList<String>>> tempComb = new HashMap<String, ArrayList<ArrayList<String>>>();
+		ArrayList<ArrayList<String>> arguments = new ArrayList<ArrayList<String>>();
+		
+		for(String )
+	}
 }
